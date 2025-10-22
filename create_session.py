37b410e7131e1d7db18681f4a959fd598e76b33e
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-22 18:10:54 +0200

    Delete create_session.py

--- create_session.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-
-api_id = 22825957
-api_hash = "bbb98a7a622f85a91a10865b6da75247"
-
-app = Client("userbot", api_id=api_id, api_hash=api_hash)
-app.start()
-print("✅ فایل سشن ساخته شد و در userbot.session ذخیره شد.")
-app.stop()
