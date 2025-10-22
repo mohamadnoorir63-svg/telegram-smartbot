c779d72bb433659b4749d4986a22d8fd515b6a53
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-22 18:16:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 from pyrogram import Client, filters
+import os
 
-# از همون فایل session استفاده کن
-app = Client("userbot.session")
+# دریافت api_id و api_hash از محیط
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+# استفاده از فایل سشن ساخته‌شده
+app = Client("userbot.session", api_id=API_ID, api_hash=API_HASH)
 
 @app.on_message(filters.command("ping"))
 def ping(_, msg):
-    msg.reply_text("✅ ربات فعاله!")
+    msg.reply_text("✅ ربات فعاله و آنلاین!")
 
 print("✅ Bot is running...")
 app.run()
