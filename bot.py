806c5a6d9de8bfd36bdecac4799250b7f850e342
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-22 18:44:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 from pyrogram import Client, filters
 import os
 
-# گرفتن api_id و api_hash از Config Vars
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
-# اتصال با سشن ساخته‌شده
 app = Client("userbot.session", api_id=API_ID, api_hash=API_HASH)
 
 @app.on_message(filters.command("ping"))
