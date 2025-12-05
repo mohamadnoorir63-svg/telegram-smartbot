cfd0289fdc2b419d5d03d017337a3d6bfb1e0d6e
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-12-05 23:06:37 +0100

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import InputStream, AudioPiped
-
-app = Client("music_bot")
-vc = PyTgCalls(app)
-
-def join_and_play(chat_id, file_path):
-    vc.join_group_call(chat_id, AudioPiped(file_path))
